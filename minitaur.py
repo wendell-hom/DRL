30bd886c2223c8ada94089f611ed5976315e1ab1
Monish <monish.ramadoss@outlook.com>
2022-02-28 19:54:32 -0500

    added a bug fix

--- minitaur.py
+++ minitaur.py
@@ -7,60 +7,70 @@ os.sys.path.insert(0, parentdir)
 
 import os
 import numpy as np
-import tensorflow as tf
+
 from pybullet_envs.minitaur.envs import minitaur_extended_env
 from pybullet_envs.minitaur.envs import minitaur_gym_env
 # from pybullet_envs.minitaur.envs import minitaur_gym_env
-from pybullet_envs.minitaur.envs.env_randomizers import minitaur_env_randomizer_from_config 
-from pybullet_envs.minitaur.envs.env_randomizers import minitaur_env_randomizer_config
+from env_randomizer import MinitaurEnvRandomizer
+
 from stable_baselines3 import PPO
 
 log_path = './logs'
 
 def main():
-  steps = 1000
-  episodes = 5
+    steps = 1000
+    episodes = 5
 
-  randomizer = minitaur_env_randomizer_from_config.MinitaurEnvRandomizerFromConfig('all_params')
-  environment = minitaur_gym_env.MinitaurGymEnv(
-      urdf_version=minitaur_gym_env.DERPY_V0_URDF_VERSION,
-      render=True,
-      leg_model_enabled=False,
-      motor_velocity_limit=np.inf,
-      pd_control_enabled=True,
-      accurate_motor_model_enabled=True,
-      motor_overheat_protection=True,
-      hard_reset=False,
-      env_randomizer=randomizer,
-      log_path=log_path)
+    randomizer = MinitaurEnvRandomizer('all_params')
+    # environment = minitaur_gym_env.MinitaurGymEnv(
+    #     urdf_version=minitaur_gym_env.DERPY_V0_URDF_VERSION,
+    #     render=True,
+    #     leg_model_enabled=False,
+    #     motor_velocity_limit=np.inf,
+    #     pd_control_enabled=True,
+    #     accurate_motor_model_enabled=True,
+    #     motor_overheat_protection=True,
+    #     hard_reset=False,
+    #     env_randomizer=randomizer,
+    #     log_path=log_path)
 
-  # environment = minitaur_gym_env.MinitaurGymEnv(
-  #     urdf_version=minitaur_gym_env.DERPY_V0_URDF_VERSION,
-  #     render=True,
-  #     num_steps_to_log=steps,
-  #     pd_latency=0.002,
-  #     control_latency=0.02,
-  #     remove_default_joint_damping=True,
-  #     on_rack=False,
-  #     env_randomizer=randomizer,
-  #     log_path=log_path)
-  # np.random.seed(100)
-  model = PPO("MlpPolicy", environment, verbose=1)
+    environment = minitaur_extended_env.MinitaurExtendedEnv(
+        history_length=1,
+        history_include_actions=True,
+        history_include_states=False,
+        include_state_difference=False,
+        include_second_state_difference=False,
+        include_base_position=False,
+        include_leg_model=False,
+        never_terminate=False,
+        action_scale=0.5,
+        urdf_version=minitaur_gym_env.DERPY_V0_URDF_VERSION,
+        render=True,
+        # leg_model_enabled=False,
+        # motor_velocity_limit=np.inf,
+        # pd_control_enabled=True,
+        # accurate_motor_model_enabled=True,
+        # motor_overheat_protection=True,
+        # hard_reset=False,
+        env_randomizer=randomizer,
+    )
 
-  observation = environment.reset()
-  print(observation)
-  for i in range(episodes):
-    sum_reward = 0
+    model = PPO("MlpPolicy", environment, verbose=1)
+
     observation = environment.reset()
-    for _ in range(steps):
-      # Sleep to prevent serial buffer overflow on microcontroller.
-      time.sleep(0.002)
-      action = environment.action_space.sample()
-      observation, reward, done, _ = environment.step(action)
-      sum_reward += reward
-      if done:
-        break
- 
+    print(observation)
+    for i in range(episodes):
+        sum_reward = 0
+        observation = environment.reset()
+        for _ in range(steps):
+        # Sleep to prevent serial buffer overflow on microcontroller.
+            time.sleep(0.002)
+            action = environment.action_space.sample()
+            observation, reward, done, _ = environment.step(action)
+            sum_reward += reward
+            if done:
+                break
+        
 
 
 if __name__ == "__main__":
